511c7999c9bff84f85d95203b6c1c2a62784d99e
Furkan Portakal <furkanportakalx@gmail.com>
2021-05-23 17:26:43 +0300

    cleaned log

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,6 @@ exports.findAll = function (req, res) {
 
 exports.create = function (req, res) {
   const newuser = new USER(req.body)
-  console.log('req.body =>', req.body)
 
   USER.create(newuser, function (err, user) {
     if (err) {
