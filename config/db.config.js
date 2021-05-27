17b3002a4daf660cb2059e13b3956decd1db0d2d
BijanE <bijanetessam@yahoo.com>
2021-05-27 06:05:42 +0300

    update 6

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,10 @@ const mysql = require('mysql')
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host: "eu-cdbr-west-01.cleardb.com",
-  user: "bde761772ee67e",
-  password: "7851fb7e",
-  database: "heroku_cee269e8c907bb2"
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB
 })
 dbConn.connect(function (err) {
   console.log('Database Connected!')
