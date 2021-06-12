3624af0cf1c1d5e61d10099f02e02acb882d772e
BijanE <bijanetessam@yahoo.com>
2021-06-12 14:54:12 +0300

    updated routes

--- src/routes/user_book.routes.js
+++ src/routes/user_book.routes.js
@@ -5,4 +5,13 @@ const user_bookController = require('../controllers/user_book.controller')
 // ADD A BOOK TO A USER
 router.post('/', user_bookController.addbook)
 
+// DELETE A BOOK FROM A USER
+router.delete('/:id', user_bookController.delete)
+
+// DELETE ALL BOOKS FROM A USER
+router.delete('/:id', user_bookController.deleteAll)
+
+// SELECT ALL BOOKS OF A USER
+router.get('/:id', user_bookController.findAll)
+
 module.exports = router
