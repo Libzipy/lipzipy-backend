98290eec35331a422be238c40bb6e1509622cea3
Bijan Etesam <56190823+BijanE@users.noreply.github.com>
2021-11-28 16:46:52 +0300

    The End

--- config/db.config.js
+++ config/db.config.js
@@ -4,11 +4,11 @@ const mysql = require('mysql')
 
 //local mysql db connection
 const dbConn = mysql.createPool({
-  connectionLimit: 100,
-  host: 'eu-cdbr-west-01.cleardb.com',
-  user: 'bde761772ee67e',
-  password: '7851fb7e',
-  database: 'heroku_cee269e8c907bb2',
+  connectionLimit: LIMIT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
   debug: false
 })
 
